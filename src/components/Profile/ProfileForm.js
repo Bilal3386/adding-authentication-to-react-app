b91bea51863d9cf12ffcd79ecc17e9df46507934
mohdbilal <mohdbilal3386@gmail.com>
2022-09-23 18:58:14 +0530

    updating the old password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,60 @@
-import classes from './ProfileForm.module.css';
+import React, { useContext, useRef, useState } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const inputPasswordRef = useRef("");
+  const authCtx = useContext(AuthContext);
+  const [isLoader, setIsLoader] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredPassword = inputPasswordRef.current.value;
+    setIsLoader(true)
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDlFTs44FMavcOEn87pLCRzq-7wWaU8FGE",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(res=> {
+      setIsLoader(false)
+      if(res.ok)
+      {
+        return res.json()
+      }
+      else
+      {
+        res.json().then((data) => {
+          throw new Error(data.error.message);
+        });
+      }
+    }).then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      alert(err)
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={inputPasswordRef} />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+       {!isLoader && <button>Change Password</button>} 
+       {isLoader && <p>Sending request....</p>}
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
